d79bf3cfa201a556be7786e3d25e84d12da79f00
millerladino <millerladino@gmail.com>
2024-04-28 13:18:34 -0500

    agregada libreria de drag and drop

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,12 +1,18 @@
-
-
-const TodoItem = ({todo,borrarTodo}) => {
+const TodoItem = ({ todo, borrarTodo }) => {
   return (
-    <li className=' py-5'>
-        <span className=' bg-amber-200 w-10 mt-10 font-bold'>{todo}</span>
-        <button className='mx-5' onClick={()=> borrarTodo(todo)}>🗑️</button>
-    </li>
-  )
-}
+    <div className=" flex">
+      <img className="w-6" src="..//../assets/images/drag.svg" alt="logo" />
+      <li className="w-full flex justify-center items-center px-5 py-5 border-2 rounded-lg bg-gray-200">
+        <span className="font-bold">{todo}</span>
+      </li>
+      <button
+        className="mx-5 border-2 rounded-lg p-2"
+        onClick={() => borrarTodo(todo)}
+      >
+        ❌
+      </button>
+    </div>
+  );
+};
 
-export default TodoItem
+export default TodoItem;
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
 import TodoItem from "../TodoItem/TodoItem";
+import { Reorder } from "framer-motion";
 
 const TodoList = () => {
     const [inputValue, setInputValue] = useState("");
@@ -32,26 +33,30 @@ const TodoList = () => {
     },[todos]); 
 
   return (
-    <>
-    <h1 className="text-center text-3xl font-bold  ">ToDo List App</h1>
-    <form className="flex justify-center flex place-items-center" onSubmit={handlerSubmit}>
+    <div className="">
+    <h1 className="text-center text-7xl font-bold  ">ToDo List App</h1>
+    <form className="flex justify-center place-items-center" onSubmit={handlerSubmit}>
         <input className="p-3 border-2 rounded-lg my-3 mx-3" type="text" placeholder="Agregar ToDos a la lista" value={inputValue} onChange={(e)=>setInputValue(e.target.value)} />
         <button className="bg-blue-500 text-white border-2 rounded-lg py-3 px-5" type="submit">Agregar</button>
     </form>
 
-    <ul className="mx-auto w-[400px] my-4 rounded-lg text-center flex flex-col py-3 border-solid border-y ">
+    <ol className="mx-auto w-[400px] my-4 rounded-lg list-disc text-center flex flex-col py-3 border-solid border-y ">
         Tareas Agregadas: {todos.length}
+        <Reorder.Group values={todos} onReorder={setTodos} >
         {todos.map((todo,index)=>(
+            <Reorder.Item value={todo} key={index} className="py-5">
             <TodoItem
             key={index}
             todo={todo}
             borrarTodo={borrarTodo}
             />
+            </Reorder.Item> 
         ))}
-    </ul>
-    </>
+        </Reorder.Group>
+    </ol>
+    </div>
   )
-}
+} 
 
 export default TodoList
 
